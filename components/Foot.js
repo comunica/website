90f6abedede13a266ff8c5d18e7a3e314e9cd48a
Ruben Taelman <ruben.taelman@ugent.be>
2020-07-16 13:56:03 +0200

    Improve a display

--- components/Foot.js
+++ components/Foot.js
@@ -1,7 +1,7 @@
 export default () => (
   <footer>
-    &copy;2018 – {(new Date().getFullYear())}&nbsp;<a href="http://www.ugent.be/">Ghent University</a>&nbsp;–&nbsp;<a href="http://www.imec.be/">imec</a>, Belgium.
-    &nbsp;
-    <a href="https://github.com/comunica/website">Source of this website.</a>
+    &copy;2018 – {(new Date().getFullYear())}&nbsp;<a href="http://www.ugent.be/">Ghent University</a>&nbsp;–&nbsp;<a href="http://www.imec.be/">imec</a>, Belgium
+    <br />
+    <a href="https://github.com/comunica/website">Source of this website</a>
   </footer>
 );
