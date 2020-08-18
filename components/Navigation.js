a3c46019f51b56f9193fd21926ec45ccc2fc7130
Ruben Taelman <ruben.taelman@ugent.be>
2020-08-18 16:03:46 +0200

    Add dedicated navigation toggle on small screens

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,14 +1,23 @@
-export default () => (
-    <nav>
-        <a href="/"><img src="/img/comunica_white.svg" className="nav-icon" /></a>
-        <ul>
-            <li><a href="http://query.linkeddatafragments.org/">Try live</a></li>
-            <li><a href="/docs/">Docs</a></li>
-            <li><a href="/contribute/">Contribute</a></li>
-            <li><a href="/about/">About</a></li>
-            <li><a href="/ask/">Ask</a></li>
-            <li><a href="/cite/">Cite</a></li>
-            <li><a href="https://github.com/comunica/comunica">GitHub</a></li>
-        </ul>
-    </nav>
-);
+export default class Navigation extends React.Component {
+    render() {
+        return <nav>
+            <div>
+                <a href="/"><img src="/img/comunica_white.svg" className="nav-icon" /></a>
+                <a href="#" className="toggle-nav"><img src="/img/navigation-toggle.svg" /></a>
+            </div>
+            <ul>
+                <li><a href="http://query.linkeddatafragments.org/">Try live</a></li>
+                <li><a href="/docs/">Docs</a></li>
+                <li><a href="/contribute/">Contribute</a></li>
+                <li><a href="/about/">About</a></li>
+                <li><a href="/ask/">Ask</a></li>
+                <li><a href="/cite/">Cite</a></li>
+                <li><a href="https://github.com/comunica/comunica">GitHub</a></li>
+            </ul>
+        </nav>;
+    }
+
+    componentDidMount() {
+        // This is not being called for some reason, see _app.js
+    }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
 import '../styles/main.scss'
 import '../node_modules/highlight.js/styles/github.css';
 
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+export default class MyApp extends React.Component {
+
+    render() {
+        const { Component, pageProps } = this.props;
+        return <Component {...pageProps} />
+    }
+
+    componentDidMount() {
+        // This should actually be in our Navigation component, but is not being called there for some reason
+        const nav = document.querySelector('nav');
+        const toggle = document.querySelector('.toggle-nav');
+        toggle.addEventListener('click', (event) => {
+            if (nav.classList.contains('active')) {
+                nav.classList.remove('active');
+            } else {
+                nav.classList.add('active');
+            }
+
+            event.preventDefault();
+            return false;
+        }, false);
+    }
 }
