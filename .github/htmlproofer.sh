9aad210e966d97c0626bc8ee3313b55996000bc2
Ruben Taelman <ruben.taelman@ugent.be>
2021-05-27 08:47:37 +0200

    Disable external link check

--- .github/htmlproofer.sh
+++ .github/htmlproofer.sh
@@ -6,4 +6,5 @@ htmlproofer ./out/ \
     --check-html true \
     --http-status-ignore "429" \
     --url-ignore "https://query.2020.eswc-conferences.org/,https://2019.eswc-conferences.org/" \
+    --disable_external true \
     --extension .html
