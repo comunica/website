978affde3c47dc5e280382234a171cb0c421c2e0
rubensworks <rubensworks@users.noreply.github.com>
2023-02-27 14:50:34 +0000

    Deploying to gh-pages from  @ c51beec197bbc0a9b8804690bbf45e50411880a4 🚀

--- _next/static/git-sha-530a4aaf9dcc8830f350cae0bf4c52d4aabba7b9/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fassociation\u002Fbounties","\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/git-sha-c51beec197bbc0a9b8804690bbf45e50411880a4/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[...slug]","\u002Fassociation\u002Fbounties"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
